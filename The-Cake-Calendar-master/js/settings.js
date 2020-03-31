23f90325e1b96352c41f9a175dc212766b96c161
julieaem <61191647+julieaem@users.noreply.github.com>
2020-03-31 20:54:37 +0200

    iets in settings

--- The-Cake-Calendar-master/js/settings.js
+++ The-Cake-Calendar-master/js/settings.js
@@ -17,3 +17,21 @@ window.onclick = function(event) {
     }
   }
 }// JavaScript Document
+
+function myFunction() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}// JavaScript Document
